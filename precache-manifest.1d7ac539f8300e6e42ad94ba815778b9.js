8d92544592cf0c307da796cbfcb5c79a453b0ef2
WK <741341649@qq.com>
2020-04-25 19:55:14 +0800

    deploy

---
+++ precache-manifest.1d7ac539f8300e6e42ad94ba815778b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387ecba5f30408fdee80c2818d67e7fa",
+    "url": "/accounting-items-2-website/QR-code.png"
+  },
+  {
+    "revision": "fe3c0ba3079f1f154be3",
+    "url": "/accounting-items-2-website/css/app.0055ce76.css"
+  },
+  {
+    "revision": "387ecba5f30408fdee80c2818d67e7fa",
+    "url": "/accounting-items-2-website/img/QR-code.387ecba5.png"
+  },
+  {
+    "revision": "4b3520ba876d4656398ded23a7cbac02",
+    "url": "/accounting-items-2-website/index.html"
+  },
+  {
+    "revision": "fe3c0ba3079f1f154be3",
+    "url": "/accounting-items-2-website/js/app.50b71815.js"
+  },
+  {
+    "revision": "6fe6e5f0c0aaf8655964",
+    "url": "/accounting-items-2-website/js/chunk-vendors.971d8d04.js"
+  },
+  {
+    "revision": "e88190ea1941fb70ff8fd2f2877c95b8",
+    "url": "/accounting-items-2-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounting-items-2-website/robots.txt"
+  }
+]);
